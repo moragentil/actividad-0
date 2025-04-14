4d4fb4fd55ff3790411ec42d15ee89424010ecd9
moragentil <moragentil@gmail.com>
2025-04-14 17:26:56 -0300

    [Parte 3.3] Levantar un error 404

--- tutorial_django/polls/views.py
+++ tutorial_django/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
+from django.http import Http404
 
 
 def index(request):
@@ -9,8 +10,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
